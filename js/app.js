54121736196212db0e087e05673b6cbe2f9ce8da
Stephanemdz <stephanemodzinu@gmail.com>
2024-04-30 15:33:25 +0200

    new foreach

--- js/app.js
+++ js/app.js
@@ -27,7 +27,31 @@ while (i< langs.length){
 langs.forEach(element => {
     console.log(element)
 });
+let prix = 55 //ht
+let taxe = 5.5 //t
+console.log(`le prix ttc est de ${prix * taxe / 100 +prix}`)
+  
+const product=[
+    {name: "car", price: 6000},
+    {name: "Bike", price: 150},
+    {name: "phone", price: 700},
+    {name: "Watch", price: 200},
+    {name: "pen", price: 8},
+    {name: "bag", price: 50},
+];
+console.log(Object.values(product[1]))
+console.log(product[1])
+product.forEach(element => {
+    console.log(element.name+" "+element.price)
+});
+for(let data of product){
+    console.log(data.name)
+}
 
+
+const filterRedItems = product.filter((row) => row.price  < 100)
+//ici il va m'afficher pour les prix inbferieur a 100 grace a la ligne d'en haut.
+console.table(filterRedItems)
 })()
 //cours_es6
 
